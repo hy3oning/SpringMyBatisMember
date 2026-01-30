320e4a36e4d8ce1e63d5add07beaae0e3d629abc
hy3oning <shyun5635@gmail.com>
2026-01-30 15:10:24 +0900

    [update] create || list  update

--- src/main/java/com/zeus/service/MemberServiceImpl.java
+++ src/main/java/com/zeus/service/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.zeus.domain.Member;
+import com.zeus.domain.MemberAuth;
 import com.zeus.mapper.MemberMapper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +18,18 @@ public class MemberServiceImpl implements MemberService {
 
 	@Autowired
 	private MemberMapper mapper;
-	
+
 	@Override
 	@Transactional
 	public int register(Member member) throws Exception {
-		return 0;
+		int count = mapper.create(member);
+		if (count > 0) {
+			MemberAuth memberAuth = new MemberAuth();
+			memberAuth.setNo(member.getNo());
+			memberAuth.setAuth("ROLE_USER");
+			mapper.createAuth(memberAuth);
+		}
+		return count;
 	}
 
 	@Override
@@ -38,7 +46,7 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public List<Member> list() throws Exception {
-		return null;
+		return mapper.list();
 	}
 
 	@Override
