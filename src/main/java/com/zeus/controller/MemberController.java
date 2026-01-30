c80484fa625becc3addd461f3bdbe90bb85b0228
hy3oning <shyun5635@gmail.com>
2026-01-30 14:21:34 +0900

    [create] MemberController Create

---
+++ src/main/java/com/zeus/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.zeus.controller;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zeus.service.MemberService;
+
+@Controller
+@MapperScan(basePackages = "com.zeus.mapper")
+@RequestMapping("/member")
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+}
